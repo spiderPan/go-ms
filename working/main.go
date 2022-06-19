b8d410c8581d8faa865eab53f7eb539d8ace0403
Pan <panbanglanfeng@gmail.com>
2022-06-19 15:47:27 -0400

    abstract the HTTP handler so that each request has its own file

--- working/main.go
+++ working/main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"go-ms/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World!")
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
-		d, err := ioutil.ReadAll(r.Body)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "Ooops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
-		// log.Printf("Data %s Error %s", d, err)
+	}()
 
-		fmt.Fprintf(w, "Hello %s with error %s", d, err)
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+	l.Println("recieved terminate, graceful shutdown", sig)
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World!")
-	})
-	http.ListenAndServe(":9090", nil)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
